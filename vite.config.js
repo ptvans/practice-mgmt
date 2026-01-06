f8cd3a5c33e4a73a654edae7b72b3e5955ec8057
P Van <ptvans@gmail.com>
2026-01-05 21:05:31 -0800

    Fix: Use root base path for custom domain (www.gethelena.com)

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const base = command === 'serve' ? '/' : '/practice-mgmt/'
-  
-  return {
-    plugins: [react()],
-    base: base,
-  }
+// Using base: '/' because site is deployed to custom domain (www.gethelena.com)
+// If deploying to username.github.io/repo-name, use base: '/repo-name/' instead
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
 })
 
