685919ed2bd2436281c18ad9a153e0b4cf1b4bf7
P Van <ptvans@gmail.com>
2026-01-05 19:54:01 -0800

    troubleshoot paths

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/practice-mgmt/', // Update this to match your GitHub repo name
+  base: '/practice-mgmt/',
 })
 
