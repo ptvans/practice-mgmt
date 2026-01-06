50f19c21b81764f91cec04b302d59cc0bc152104
P Van <ptvans@gmail.com>
2026-01-05 21:03:33 -0800

    Use conditional base path for dev and production

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/practice-mgmt/',
+export default defineConfig(({ command }) => {
+  const base = command === 'serve' ? '/' : '/practice-mgmt/'
+  
+  return {
+    plugins: [react()],
+    base: base,
+  }
 })
 
